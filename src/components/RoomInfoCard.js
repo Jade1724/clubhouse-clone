a8cecb97eee9b6490e08436ac46d6dedcc1bb9bc
Jade1724 <haruka.i1724@gmail.com>
2022-07-17 22:17:00 +1200

    Added details on RoomInfoCard and styled it.

--- src/components/RoomInfoCard.js
+++ src/components/RoomInfoCard.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import style from "../style/roomCard.module.css";
+import { BsChatDots, BsChatDotsFill, BsFillPersonFill} from 'react-icons/bs';
 import data from "../data/roomCard.json";
 
 export default function RoomInfoCard(props) {
@@ -11,6 +12,25 @@ export default function RoomInfoCard(props) {
             <div className={style.roomCardContainer}>
               <h6>{item.title}</h6>
               <h2>{item.sub_title}</h2>
+              <div className={style.roomMembers}>
+                <div>
+                    <img src="images/user-image1.jpg" />
+                    <img src="images/user-image2.jpg" />
+                </div>
+                <div>
+                    {item.members.map((person) => (
+                        <p>
+                            {person.first_name} {person.last_name} <BsChatDots />
+                        </p>
+                    ))}
+                    <p className="d-flex align-items-center">
+                        <span className="me-2">1.8</span>
+                        <BsFillPersonFill />
+                        <span className="mx-2"></span>{" "}
+                        <span className="me-2">5</span> <BsChatDotsFill />
+                    </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
