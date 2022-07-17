619347391c7d9b554745027384f5f19a31598b4d
Jade1724 <haruka.i1724@gmail.com>
2022-07-17 21:48:17 +1200

    Created DailyInfoCard component and styled.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
+import style from '../style/home.module.css';
 import Header from "../components/Header";
+import DailyInforCard from "../components/DailyInfoCard";
+import RoomInfoCard from "../components/RoomInfoCard";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <div className={style.home_container}>
+        <DailyInforCard />
+        <RoomInfoCard />
+      </div>
     </>
   );
 }
