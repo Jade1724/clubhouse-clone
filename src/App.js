9f7d5e3906468fcbdb2d92f0dce53abf7c843e5c
Jade1724 <haruka.i1724@gmail.com>
2022-07-17 15:56:45 +1200

    Added Switch to the App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import PlanLayout from "./pages/layouts/PlanLayout";
 import Welcome from "./pages/Welcome";
 import PhoneConfirmation from "./pages/PhoneConfirmation";
 import CodeConfirmation from "./pages/CodeConfirmation";
-
-// <PlanLayout>
+import AllowNotification from "./pages/AllowNotification";
 
 function App() {
   return (
     <BrowserRouter>
-      <PlanLayout>
-        <Route exact path='/' component={Welcome} />
-        <Route exact path='/invite' component={PhoneConfirmation} />
-        <Route exact path='/code_confirm' component={CodeConfirmation} />
-      </PlanLayout>
+      <Route
+        exact
+        path={[
+          "/",
+          "/invite",
+          "/get_username",
+          "/code_confirm",
+          "/allow_notification",
+        ]}
+      >
+        <PlanLayout>
+          <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route exact path="/invite" component={PhoneConfirmation} />
+            <Route exact path="/code_confirm" component={CodeConfirmation} />
+            <Route
+              exact
+              path="/allow_notification"
+              component={AllowNotification}
+            />
+          </Switch>
+        </PlanLayout>
+      </Route>
     </BrowserRouter>
   );
 }
