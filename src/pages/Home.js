e720b99c98e0601df3b1a88317965b6d9148ebfa
Jade1724 <haruka.i1724@gmail.com>
2022-07-18 03:03:01 +1200

    Created BottomSheet and StartRoom component and styled them.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,57 @@
-import React from "react";
-import style from '../style/home.module.css';
+import React, { useState } from "react";
+import style from "../style/home.module.css";
 import Header from "../components/Header";
 import DailyInforCard from "../components/DailyInfoCard";
 import RoomInfoCard from "../components/RoomInfoCard";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsGrid3X3GapFill } from "react-icons/bs";
+import data from "../data/roomCard.json";
+import BottomSheet from "../components/BottomSheet";
 
 export default function Home() {
+  const [itemsVisible, setItemsVisible] = useState(true);
+  const [sheetVisible, setSheetVisible] = useState(false);
+  const [sheetCreateRoom, setSheetCreateRoom] = useState(false);
+  const [loaderVisibility, setLoaderVisibility] = useState(false);
+  const [cardId, setCardId] = useState(1);
+
   return (
     <>
+    {loaderVisibility ? (
+      <div style={{position: "fixed", top: '0', righgt: '0', left: '0', bottom: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <img src="/images/loading.gif" />
+      </div>
+    ) : (
+      ""
+    )}
       <Header />
       <div className={style.home_container}>
         <DailyInforCard />
         <RoomInfoCard />
       </div>
+      <div className={style.action_btn}>
+        <button onClick={() => setSheetVisible(true)}>
+          <AiOutlinePlus className="me-2" />
+          Start a room
+        </button>
+        <button>
+          <BsGrid3X3GapFill />
+        </button>
+      </div>
+      <BottomSheet
+        sheetTitle="start room"
+        setSheetVisible={(item) => setSheetVisible(item)}
+        sheetVisible={sheetVisible}
+        cardDetail={data.find((item) => item.id == cardId)}
+        setItemsVisible={(item) => setItemsVisible(item)}
+        setSheetCreateRoom={(item) => {
+          setLoaderVisibility(true);
+          setTimeout(() => {
+            setSheetCreateRoom(item);
+            setLoaderVisibility(false);
+          }, 1000);
+        }}
+      />
     </>
   );
 }
