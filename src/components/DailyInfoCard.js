959426afe1424bde97289ff843c2480671d10c82
Jade1724 <haruka.i1724@gmail.com>
2022-07-26 02:01:52 +1200

    Finished the NewRoom component and styling.

--- src/components/DailyInfoCard.js
+++ src/components/DailyInfoCard.js
@@ -3,11 +3,10 @@ import style from "../style/dailyInfoCard.module.css";
 import data from "../data/dailyCard.json";
 
 export default function DailyInforCard() {
-  console.log(data[0].title);
   return (
     <div className={style.dailyCard}>
       {data.map((item) => (
-        <div>
+        <div key={item.id}>
           <span className="">{item.time}</span>
           <div>
             <span>{item.title}</span>
--- src/components/bottom_sheets/NewRoom.js
+++ src/components/bottom_sheets/NewRoom.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import style from "../../style/roomDetail.module.css";
-import { AiOutlineFile } from "react-icons/ai";
-import { BsMicMuteFill } from "react-icons/bs";
+import { AiOutlineFile, AiOutlinePlus } from "react-icons/ai";
+import { BsMicMuteFill, BsMicFill } from "react-icons/bs";
 import { IoIosArrowBack } from "react-icons/io";
+import { FaHandPeace, FaHandPaper } from "react-icons/fa";
 
 export default function NewRoom(props) {
-  const [micMuteVisible, setMicMuteVisible] = useState(false);
+  const [micMuteVisible, setMicMuteVisible] = useState(true);
+  const [itemsVisible, setItemsVisible] = useState(false);
   const card = props.cardDetail;
 
   return (
@@ -19,15 +21,59 @@ export default function NewRoom(props) {
                 props.setSheetVisible(false);
               }}
             >
-              <IoIosArrowBack className={style.arrow_icon}/>
+              <IoIosArrowBack className={style.arrow_icon} />
             </a>
             <span>Hallway</span>
-            <div>
-                <AiOutlineFile />
-                <img src="/images/user-image1.jpg" className={style.profile_img}/>
-            </div>
+          </div>
+          <div>
+            <AiOutlineFile />
+            <img src="/images/user-image1.jpg" className={style.profile_img} />
           </div>
         </div>
+        <div className={style.roomDetailCard}>
+          <div
+            className="d-flex align-items-center justify-content-between flex-wrap"
+            style={{ padding: "0.5em 1em" }}
+          >
+            {card.members.map((item) => (
+              <div key={item.id} className={style.memberContainer}>
+                {micMuteVisible ? (
+                  <div className={style.audio_icon}>
+                    <BsMicMuteFill />
+                  </div>
+                ) : (
+                  ""
+                )}
+                <img src="/images/user-image2.jpg" />
+                <p>
+                  <span>*</span>
+                  {item.first_name}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={style.footer}>
+          <button onClick={() => {
+            props.setSheetVisible(false);
+          }}
+          >
+            <FaHandPeace style={{color: "gold"}}/>
+            Leave Quietly
+          </button>
+          <div>
+          <button>
+            <AiOutlinePlus />
+          </button>
+          <button>
+            <FaHandPaper style={{color: "gold"}}/>
+          </button>
+          <button onClick={() => setMicMuteVisible(!micMuteVisible)}>
+            {micMuteVisible ? <BsMicMuteFill /> : <BsMicFill />}
+          </button>
+        </div>
+        </div>
+        
       </div>
     </>
   );
