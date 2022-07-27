7f3a509e56cf826530e7c6bfe4501a482b2c6b7e
Jade1724 <haruka.i1724@gmail.com>
2022-07-28 01:05:28 +1200

    Created conversation section on Explore component,

---
+++ src/components/SubHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "../style/explore.module.css";
+import { Link } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+
+export default function SubHeader({ pageTitle }) {
+  return (
+    <div className={style.head}>
+      <Link to="/home">
+        <IoIosArrowBack />
+      </Link>
+      <h3>{pageTitle}</h3>
+    </div>
+  );
+}
--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 import style from "../style/explore.module.css";
-import { DownOutlined, FireOutLined } from "@ant-design/icons";
+import { DownOutlined, FireOutlined } from "@ant-design/icons";
+import { Input } from "antd";
 import data from "../data/explore.json";
+import { FiSearch } from "react-icons/fi";
+import SubHeader from "../components/SubHeader";
 
 export default function Explore() {
   const { people, conversation } = data;
 
   return (
     <div className={style.exploreContainer}>
+      <div className={style.header}>
+        <SubHeader pageTitle="EXPLORE"/>
+        <Input
+          style={{
+            backgroundColor: "#f4f4f4",
+            borderRadius: "0.8em",
+            padding: "0.3em 1em",
+            border: "none",
+            boxShadow: "none",
+          }}
+          size="large"
+          placeholder="Find People and Clubs"
+          prefix={<FiSearch style={{width: "15px", height: "15px"}}/>}
+        ></Input>
+      </div>
       <h6> PEOPLE TO FOLLOW</h6>
       <div className={style.peopleContainer}>
         {people.map((item) => (
@@ -23,8 +41,22 @@ export default function Explore() {
           </div>
         ))}
         <button className={style.showMore}>
-            Show more people <DownOutlined />
+          Show more people <DownOutlined />
         </button>
+      </div>
+      <h6>FIND CONVERSATIONS ABOUT ...</h6>
+      <div className="row mx-0">
+        {conversation.map((item) => (
+          <div className="col-6 px-2 mb-3">
+            <div className={style.conversationCard}>
+              <h6>
+                <FireOutlined />
+                {item.title}
+              </h6>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
